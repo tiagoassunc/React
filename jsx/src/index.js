23fad750112248994678ba297e73be9939641fa5
Tiago Assunção <tiagoassunc@gmail.com>
2021-06-09 09:46:21 -0300

    "jsx: bases"

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Enable reloading
+if (module.hot) {
+  module.hot.accept();
+}
+
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: "click me" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      {/* <label class="label" for="name"> */}
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      {/* {Inline Styling with JSX}
+      <button style= "background-color: blue; color: white"> */}
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
