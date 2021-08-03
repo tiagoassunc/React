1f5c54d0743b8912e6101d6fa2193b8fc3988dc5
Tiago Assunção <tiagoassunc@gmail.com>
2021-08-03 14:08:42 -0300

    "Updating"

---
+++ redux-api-thunk/src/actions/index.js
@@ -0,0 +1,22 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  // (dispatch, getState), function returning a function
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+// export const fetchPosts = () => {
+//   return async  (dispatch, getState) => {
+//     const response = await jsonPlaceholder.get("/posts");
+
+//     dispatch({ type: "FETCH_POSTS", payload: response });
+//   };
+// };
