87da4adaccc1673b47cb1ef014b1e9901fab03d8
Tiago Assunção <tiagoassunc@gmail.com>
2021-08-09 16:17:27 -0300

    "finishing: redux-api-thunk"

--- React/redux-api-thunk/src/actions/index.js
+++ React/redux-api-thunk/src/actions/index.js
@@ -1,4 +1,19 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  /* const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id))); */
+
+  // Chaining in lodash
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async (dispatch) => {
   // (dispatch, getState), function returning a function
@@ -8,10 +23,19 @@ export const fetchPosts = () => async (dispatch) => {
 };
 
 export const fetchUser = (id) => async (dispatch) => {
+  // Memoize
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+/* export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// Memoize
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+}); */
 
 // export const fetchPosts = () => {
 //   return async  (dispatch, getState) => {
