ebdd2d51fdb6df2cf3e0264774cc0a3e90046728
Tiago Assunção <tiagoassunc@gmail.com>
2021-09-05 21:56:39 -0300

    Updating study

--- System-Design/packages/react/src/molecules/Select/Select.stories.tsx
+++ System-Design/packages/react/src/molecules/Select/Select.stories.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 
 import Select from "./Select";
+import "@ds.e/scss/lib/Select.css";
+
+const options = [
+  {
+    label: "Strict Black",
+    value: "black",
+  },
+  {
+    label: "Heavenly Green",
+    value: "green",
+  },
+  {
+    label: "Sweet Pink",
+    value: "pink",
+  },
+];
 
 export default {
   title: "Select",
 };
 
-export const Common = () => <Select options={[]} />;
+export const Common = () => <Select options={options} />;
+
+export const RenderOption = () => (
+  <Select
+    options={options}
+    renderOption={({ getOptionRecommendedProps, option, isSelected }) => (
+      <span {...getOptionRecommendedProps()}>
+        {option.label} {isSelected ? "SELECTED!" : ""}
+      </span>
+    )}
+  />
+);
