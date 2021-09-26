c4294d4a9d4e032d01a186ba075bd8e7a48707b1
Tiago Assunção <tiagoassunc@gmail.com>
2021-09-26 17:23:33 -0300

    Update files organization

--- Pattern-Design/showcase/src/patterns/08.js
+++ Pattern-Design/showcase/src/patterns/08.js
@@ -1,20 +1,30 @@
 import React, {
   useState,
-  useEffect,
   useCallback,
   useLayoutEffect,
-  forwardRef,
   useRef,
+  useEffect,
 } from "react";
 
 import mojs from "mo-js";
 import { generateRandomNumber } from "../utils/generateRandomNumber";
 import styles from "./index.css";
-import userStyles from "./usage.css";
+
+const INITIAL_STATE = {
+  count: 0,
+  countTotal: generateRandomNumber(50, 100),
+  isClicked: false,
+};
+
+const callFnsInSequence =
+  (...fns) =>
+  (...args) => {
+    fns.forEach((fn) => fn && fn(...args));
+  };
 
 /** ====================================
    *          🔰Hook
-        Hook for Animation
+        Custom Hook for Animation
   ==================================== **/
 
 const useClapAnimation = ({
@@ -97,7 +107,7 @@ const useClapAnimation = ({
     });
 
     if (typeof burstEl === "string") {
-      const id = burstEl.slice(1, burstEl.length);
+      clap.style.transform = "scale(1, 1)";
       const el = document.getElementById(id);
       el.style.transform = "scale(1, 1)";
     } else {
@@ -120,72 +130,16 @@ const useClapAnimation = ({
 
 /** ====================================
    *          🔰Hook
-        Hook for Clap State
-  ==================================== **/
-const MAX_CLAP = 50;
-const INIT_STATE = {
-  count: 0,
-  countTotal: generateRandomNumber(500, 10000),
-  isClicked: false,
-};
-
-const useClapState = ({ initialState = INIT_STATE } = {}) => {
-  const [clapState, setClapState] = useState(initialState);
-  const { count, countTotal } = clapState;
-
-  const handleClapClick = useCallback(() => {
-    setClapState({
-      count: Math.min(count + 1, MAX_CLAP),
-      countTotal: count < MAX_CLAP ? countTotal + 1 : countTotal,
-      isClicked: true,
-    });
-  }, [count, countTotal]);
-
-  const togglerProps = {
-    onClick: handleClapClick,
-    "aria-pressed": clapState.isClicked,
-  };
-
-  const counterProps = {
-    count,
-    "aria-valuemax": MAX_CLAP,
-    "aria-valuemin": 0,
-    "aria-valuenow": count,
-  };
-
-  return {
-    clapState,
-    togglerProps,
-    counterProps,
-  };
-};
-
-/** ====================================
-   *          🔰Hook
-        useEffectAfterMount
+        Custom Hook for useDOMRef
   ==================================== **/
 
-function useEffectAfterMount(cb, deps) {
-  const componentJustMounted = useRef(true);
-  useEffect(() => {
-    if (!componentJustMounted.current) {
-      return cb();
-    }
-    componentJustMounted.current = false;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
-}
-
-/** ====================================
-   *          🔰Hook
-            useDOMRef
-  ==================================== **/
 const useDOMRef = () => {
-  const [DOMRef, setDOMRef] = useState({});
+  const [DOMRef, setRefState] = useState({});
+
   const setRef = useCallback((node) => {
     if (node !== null) {
-      setDOMRef((prevDOMRefs) => ({
-        ...prevDOMRefs,
+      setRefState((prevRefState) => ({
+        ...prevRefState,
         [node.dataset.refkey]: node,
       }));
     }
@@ -195,44 +149,84 @@ const useDOMRef = () => {
 };
 
 /** ====================================
+   *          🔰Hook
+        Custom Hook for useClapState
+  ==================================== **/
+
+const useClapState = (initialState = INITIAL_STATE) => {
+  const MAXIMUM_USER_CLAP = 50;
+  const [clapState, setClapState] = useState(initialState);
+  const { count, countTotal } = clapState;
+
+  const updateClapState = useCallback(() => {
+    setClapState(({ count, countTotal }) => ({
+      isClicked: true,
+      count: Math.min(count + 1, MAXIMUM_USER_CLAP),
+      countTotal: count < MAXIMUM_USER_CLAP ? countTotal + 1 : countTotal,
+    }));
+  }, [count, countTotal]);
+
+  // props collection for 'click'
+  const getTogglerProps = ({ onClick, ...otherProps } = {}) => ({
+    onClick: callFnsInSequence(updateClapState, onClick),
+    "aria-pressed": clapState.isClicked,
+    ...otherProps,
+  });
+
+  // props collection for 'count'
+  const getCounterProps = ({ ...otherProps }) => ({
+    count,
+    "aria-valuemax": MAXIMUM_USER_CLAP,
+    "aria-valuemin": 0,
+    "aria-valuenow": count,
+    ...otherProps,
+  });
+
+  return { clapState, updateClapState, getTogglerProps, getCounterProps };
+};
+
+/** ====================================
+   *          🔰Hook
+    Custom Hook for useEffectAfterMount
+  ==================================== **/
+
+const useEffectAfterMount = (cb, deps) => {
+  const componentJustMounted = useRef(true);
+  useEffect(() => {
+    if (!componentJustMounted.current) {
+      return cb();
+    }
+    componentJustMounted.current = false;
+    // eslint-disable-next line react-hooks/exhasutive-deps
+  }, deps);
+};
+
+/** ====================================
    *      🔰SubComponents
   Smaller Component used by <MediumClap />
   ==================================== **/
 
-const ClapContainer = forwardRef(
-  (
-    { children, handleClick, className, style: userStyles = {}, ...restProps },
-    ref
-  ) => {
-    const classNames = [styles.clap, className].join(" ").trim();
+const ClapContainer = ({ children, setRef, handleClick, ...restProps }) => {
+  return (
+    <button
+      ref={setRef}
+      className={styles.clap}
+      onClick={handleClick}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+};
 
-    return (
-      <button
-        onClick={handleClick}
-        className={classNames}
-        style={userStyles}
-        ref={ref}
-        {...restProps}
-      >
-        {children}
-      </button>
-    );
-  }
-);
-
-const ClapIcon = ({ className = "", style: userStyles = {}, isClicked }) => {
-  const classNames = [styles.icon, isClicked ? styles.checked : "", className]
-    .join(" ")
-    .trim();
-
+const ClapIcon = ({ isClicked }) => {
   return (
     <span>
       <svg
         id="clapIcon"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="-549 338 100.1 125"
-        className={classNames}
-        style={userStyles}
+        className={`${styles.icon} ${isClicked && styles.checked}`}
       >
         <path d="M-471.2 366.8c1.2 1.1 1.9 2.6 2.3 4.1.4-.3.8-.5 1.2-.7 1-1.9.7-4.3-1-5.9-2-1.9-5.2-1.9-7.2.1l-.2.2c1.8.1 3.6.9 4.9 2.2zm-28.8 14c.4.9.7 1.9.8 3.1l16.5-16.9c.6-.6 1.4-1.1 2.1-1.5 1-1.9.7-4.4-.9-6-2-1.9-5.2-1.9-7.2.1l-15.5 15.9c2.3 2.2 3.1 3 4.2 5.3zm-38.9 39.7c-.1-8.9 3.2-17.2 9.4-23.6l18.6-19c.7-2 .5-4.1-.1-5.3-.8-1.8-1.3-2.3-3.6-4.5l-20.9 21.4c-10.6 10.8-11.2 27.6-2.3 39.3-.6-2.6-1-5.4-1.1-8.3z" />
         <path d="M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9" />
@@ -240,33 +234,20 @@ const ClapIcon = ({ className = "", style: userStyles = {}, isClicked }) => {
     </span>
   );
 };
-
-const ClapCount = forwardRef(
-  ({ count, className = "", style: userStyles = {}, ...restProps }, ref) => {
-    const classNames = [styles.count, className].join(" ").trim();
-
-    return (
-      <span ref={ref} className={classNames} style={userStyles} {...restProps}>
-        +{count}
-      </span>
-    );
-  }
-);
-
-const CountTotal = forwardRef(
-  (
-    { countTotal, className = "", style: userStyles = {}, ...restProps },
-    ref
-  ) => {
-    const classNames = [styles.total, className].join(" ").trim();
-
-    return (
-      <span ref={ref} className={classNames} style={userStyles} {...restProps}>
-        {countTotal}
-      </span>
-    );
-  }
-);
+const ClapCount = ({ count, setRef, ...restProps }) => {
+  return (
+    <span ref={setRef} className={styles.count} {...restProps}>
+      +{count}
+    </span>
+  );
+};
+const CountTotal = ({ countTotal, setRef, ...restProps }) => {
+  return (
+    <span ref={setRef} className={styles.total} {...restProps}>
+      {countTotal}
+    </span>
+  );
+};
 
 /** ====================================
       *        🔰USAGE
@@ -275,35 +256,41 @@ const CountTotal = forwardRef(
   ==================================== **/
 
 const Usage = () => {
-  const { clapState, togglerProps, counterProps } = useClapState();
+  const { clapState, getTogglerProps, getCounterProps } = useClapState();
   const { count, countTotal, isClicked } = clapState;
-
-  const [{ clapContainerRef, clapCountRef, countTotalRef }, setRef] =
-    useDOMRef();
+  const [{ clapRef, clapCountRef, clapTotalRef }, setRef] = useDOMRef();
 
   const animationTimeline = useClapAnimation({
     duration: 300,
     bounceEl: clapCountRef,
-    fadeEl: countTotalRef,
-    burstEl: clapContainerRef,
+    fadeEl: clapTotalRef,
+    burstEl: clapRef,
   });
 
   useEffectAfterMount(() => {
     animationTimeline.replay();
   }, [count]);
 
+  const handleClick = () => {
+    console.log("CLICKED !!!");
+  };
+
   return (
     <ClapContainer
-      ref={setRef}
-      data-refkey="clapContainerRef"
-      {...togglerProps}
+      setRef={setRef}
+      data-refkey="clapRef"
+      {...getTogglerProps({ onClick: handleClick, "aria-pressed": false })}
     >
       <ClapIcon isClicked={isClicked} />
-      <ClapCount ref={setRef} data-refkey="clapCountRef" {...counterProps} />
+      <ClapCount
+        setRef={setRef}
+        data-refkey="clapCountRef"
+        {...getCounterProps()}
+      />
       <CountTotal
-        ref={setRef}
-        data-refkey="countTotalRef"
         countTotal={countTotal}
+        setRef={setRef}
+        data-refkey="clapTotalRef"
       />
     </ClapContainer>
   );
